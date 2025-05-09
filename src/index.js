3a3fa17ebff625a34f2c0c5370fcdb788eede17b
iamsagarkonada <sagar.konda3@gmail.com>
2025-05-09 16:53:59 +0530

    Connecting to DB in Various Styles of Coding

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from 'express'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const app=express()
+
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR : ",error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is Listing on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Error: ",error)
+        
+    }
+})()
+
+*/
