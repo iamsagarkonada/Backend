410e37d4a4e8c8c82047570824a474a2cd5fe074
iamsagarkonada <sagar.konda3@gmail.com>
2025-05-22 11:54:44 +0530

    User and Video Models

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
+import dotenv from 'dotenv'
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import { DB_NAME } from "./constants.js";
 import express from 'express'
+import connectDB from './db/index.js';
 
+
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is Running at Port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((e)=>{
+    console.log("MONGODB Connection Failed");
+    
+})
 
 
 
